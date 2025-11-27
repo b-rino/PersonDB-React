96351d3cf5b7e1a48b99a1944f6efcbe225004a2
Benjamin Rino <benjar.rino@gmail.com>
2025-11-27 14:04:43 +0100

    delete function done and controlled components added in forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
 import "./styles/App.css";
+import PersonForm from "./components/PersonForm";
+import PersonList from "./components/PersonList";
+import React, { useState, useEffect } from "react";
+import { fetchData } from "./util/persistence";
+
+const blankPerson = {
+  id: "",
+  age: "",
+  name: "",
+  email: "",
+  gender: "",
+};
 
 function App() {
+  const [persons, setPersons] = useState([]);
+  const [personToEdit, setPersonToEdit] = useState(blankPerson);
+
+  const APIURL = "http://localhost:3000/api";
+
+  function editPerson(person) {
+    setPersonToEdit(person);
+  }
+
+  function getPersons(callback) {
+    fetchData(APIURL, callback);
+  }
+
+  function deletePersonById(id) {
+    //Fjern fra API
+    fetchData(`${APIURL}/${id}`, () => {}, "DELETE");
+    //Fjern fra persons array
+    setPersons([...persons].filter((person) => person.id != id));
+  }
+
+  useEffect(() => {
+    getPersons((data) => setPersons(data));
+  }, []);
+
   return (
     <div className="container">
       <h1>Person DB</h1>
+      <PersonForm blankPerson={blankPerson} personToEdit={personToEdit} />
+      <PersonList
+        persons={persons}
+        deletePersonById={deletePersonById}
+        editPerson={editPerson}
+      />
     </div>
   );
 }
---
+++ src/components/PersonForm.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+
+function PersonForm({ blankPerson, personToEdit }) {
+  const [person, setPerson] = useState({ ...personToEdit });
+
+  useEffect(() => {
+    setPerson(personToEdit);
+  }, [personToEdit]);
+
+  function handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setPerson({ ...person, [name]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault(); //for at stoppe event bubbling, da enhver button har submit-funktion som default!
+    alert("submit", person);
+    //callback funktion fra App.jsx, som enten indsætter eller opdaterer en person (alt efter om id er tomt eller ej)
+  }
+
+  return (
+    <div>
+      <h1>Add/Edit Person</h1>
+      {JSON.stringify(person)} //Debugging purposes
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="id">Id</label>
+        <input
+          name="id"
+          id="id"
+          type="number"
+          readOnly
+          placeholder="id"
+          value={person.id}
+        />
+        <label htmlFor="name">Name</label>
+        <input
+          name="name"
+          id="name"
+          type="text"
+          placeholder="name"
+          value={person.name}
+          onChange={handleChange}
+        />
+        <label htmlFor="age">Age</label>
+        <input
+          name="age"
+          id="age"
+          type="number"
+          min="1"
+          max="120"
+          placeholder="age"
+          value={person.age}
+          onChange={handleChange}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          id="email"
+          type="email"
+          placeholder="email"
+          value={person.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="gender">Gender</label>
+        <select
+          name="gender"
+          id="gender"
+          value={person.gender}
+          onChange={handleChange}
+        >
+          <option defaultChecked>Select Gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+      <button>Update</button>
+      <button onClick={() => setPerson(blankPerson)}>Reset</button>
+    </div>
+  );
+}
+
+export default PersonForm;
